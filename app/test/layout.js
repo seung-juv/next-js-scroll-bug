6c448b2b9653816864304d7fb6d36b766e8e8715
Seung Ju <seung-ju@seung-ju.com>
2024-05-25 16:14:16 +0900

    initial commit

---
+++ app/test/layout.js
@@ -0,0 +1,5 @@
+export default function RootLayout({children}) {
+    return (
+        <div style={{minHeight: '90vh', paddingTop: '50vh'}}>{children}</div>
+    );
+}
---
+++ app/test2/page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Page() {
+    return (
+        <div style={{height: '100vh'}}>
+            test
+        </div>
+    )
+}
